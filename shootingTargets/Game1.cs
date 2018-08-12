2ff2ed4aa5aeec57ef8e327899b50b75c0f18b23
Halim Lais <halimlais@hotmail.com>
2018-08-12 21:17:16 +0100

    Shooting Game (MonoGame)
    MonoGame - first project a shooting game

--- shootingTargets/Game1.cs
+++ shootingTargets/Game1.cs
@@ -4,26 +4,40 @@ using Microsoft.Xna.Framework.Input;
 
 namespace shootingTargets
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D target_Sprite;
+        Texture2D crosshair_Sprite;
+        Texture2D background_Spirte;
+
+        SpriteFont gameFont;
+
+        Vector2 targetPosition = new Vector2(0,0);
+        Vector2 mousePosition;
+        float mouseTargetDist;
+
+        const int TARGET_RADIUS = 45;
+
+        MouseState mState;
+        bool mReleased = true;
+
+
+
+        int score = 0;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -31,53 +45,89 @@ namespace shootingTargets
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            target_Sprite = Content.Load<Texture2D>("target");
+            crosshair_Sprite = Content.Load<Texture2D>("crosshairs");
+            background_Spirte = Content.Load<Texture2D>("sky");
+
+            gameFont = Content.Load<SpriteFont>("galleryFont");
+
+
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+
+
+
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            mousePosition = new Vector2(mState.X, mState.Y);
 
-            // TODO: Add your update logic here
+            mouseTargetDist = Vector2.Distance(targetPosition, mousePosition);
+            ClickingMouse();
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        private void ClickingMouse()
+        {
+            mState = Mouse.GetState();
+            if (mState.LeftButton == ButtonState.Pressed && mReleased == true)
+            {
+                if (mouseTargetDist < TARGET_RADIUS)
+                {
+                    score++;
+                }
+                mReleased = false;
+
+            }
+            if (mState.LeftButton == ButtonState.Released)
+            {
+                mReleased = true;
+            }
+        }
+      
+            
+
+
+
+      
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            spriteBatch.Draw( background_Spirte, new Vector2(0, 0), Color.White);
+            spriteBatch.DrawString(gameFont, $"Score: {score}", new Vector2(0, 0), Color.White);
+            spriteBatch.DrawString(gameFont, $"Mouse {mousePosition}", new Vector2(20, 20), Color.White);
+            spriteBatch.Draw(target_Sprite, new Vector2( targetPosition.X - TARGET_RADIUS, targetPosition.Y - TARGET_RADIUS), Color.White);
+            
+            
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
+
+        //private bool MouseOnTarger()
+        //{
+
+            
+           
+
+            
+        //}
     }
 }
